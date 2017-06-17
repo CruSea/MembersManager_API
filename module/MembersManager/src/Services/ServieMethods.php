0d0b986393641f831cc7f65bc96ba571f0115097
BENGEOS <george.beng@gmail.com>
2017-06-17 15:41:35 +0300

    add some changes'

--- module/MembersManager/src/Services/ServieMethods.php
+++ module/MembersManager/src/Services/ServieMethods.php
@@ -9,7 +9,29 @@
 namespace MembersManager\Services;
 
 
+use MembersManager\Entities\Privilege;
+use MembersManager\Entities\User;
+
 interface ServieMethods
 {
+    /**
+     * User Table
+     * @return mixed
+     */
+    public function addUser(User $user);
+    public function getUser(User $user);
+    public function getUserByEmail(User $user);
+    public function getUserByID(User $user);
+    public function checkUser(User $user);
+    public function updateUser(User $user);
+    public function removeUser(User $user);
 
+    /**
+     * Company Privilege
+     * @return mixed
+     */
+    public function addPrivilege(Privilege $privilege);
+    public function getPrivilege(Privilege $privilege);
+    public function getAllPrivilege();
+    public function getLessPrivilege(Privilege $privilege);
 }
