5e39c49f7d9952a8e8431c0d76509a7a94b9257b
BENGEOS <george.beng@gmail.com>
2017-07-31 16:10:10 +0300

    update port name resolution

--- bootstrap.php
+++ bootstrap.php
@@ -29,7 +29,7 @@ $conn = array(
     'password' => 'passben',
 
 //    'host' => 'localhost:8889', // Localserver
-    'host' => 'localhost', // IMS server
+    'host' => 'localhost:3306', // IMS server
 
 
     'driver' => 'pdo_mysql',
